2ba45ce0c1c1921fc2e3c41ac99fbc118bfb8195
Valentine <valentine.paramonov@gmail.com>
2016-11-19 05:18:56 +0200

    Start rethinkdb from docker

--- update-fortune.sh
+++ update-fortune.sh
@@ -2,6 +2,11 @@
 set -o errexit
 
 yarn run transpile
+docker start rethinkdb
+while ! curl -sf localhost:8080; do 
+   sleep 3
+done
 yarn run scrape
 yarn run make-fortune
+docker stop rethinkdb
 yarn run install-fortune
