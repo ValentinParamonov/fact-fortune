baeedd9f3250d5ada4565cc6db8c254a28267db2
valentine <valentin.paramonov@protonmail.com>
2017-10-02 16:18:04 +0300

    Refactor

--- fortune-maker.js
+++ fortune-maker.js
@@ -1,56 +1,66 @@
-import fs from 'fs'
-import r from 'rethinkdb'
+import fs from 'fs';
+import r from 'rethinkdb';
 
 const connectionConfig = {
     host: 'localhost',
     port: 28015,
     pathname: '/',
     db: 'factslides'
-}
+};
 
-const out = fs.createWriteStream('factslides')
-out.once('open', () => {
-    r.connect(connectionConfig)
-     .then(retrieveFacts)
-     .then(writeFactsTo(out))
-     .catch(console.error)
-})
+const out = fs.createWriteStream('factslides');
+out
+    .once('open', () => {
+        r
+            .connect(connectionConfig)
+            .then(retrieveFacts)
+            .then(writeFactsTo(out))
+            .catch(console.error);
+    });
 
 function retrieveFacts(connection) {
-    return r.table('facts')
-            .orderBy({index: r.asc('id')})
-            .run(connection)
+    return r
+        .table('facts')
+        .orderBy({index: r.asc('id')})
+        .run(connection);
 }
 
 function writeFactsTo(out) {
-    const writeFact = writeFactTo(out)
+    const writeFact = writeFactTo(out);
     return cursor => {
-        out.cork()
-        cursor.each((error, fact) => {
-            if (error) return console.error(error)
-            writeFact(fact.text)
-        },
-        () => {
-            out.uncork()
-            out.end()
-            process.exit(0)
-        })
-    }
+        out.cork();
+        cursor
+            .each((error, fact) => {
+                    if (error) return console.error(error);
+                    writeFact(fact.text);
+                },
+                () => {
+                    out.uncork();
+                    out.end();
+                    process.exit(0);
+                }
+            );
+    };
 }
 
 function writeFactTo(out) {
     return factText => {
-        out.write(withLineWidth(factText, 75) + '\n')
-        out.write('%\n')
-    }
+        out.write(withLineWidth(factText, 75) + '\n');
+        out.write('%\n');
+    };
 }
 
 function withLineWidth(text, width) {
-    return text.split(/\s/).reduce((a,b) => {
-        const init = a.slice(0,-1)
-        const [last] = a.slice(-1)
-        return last.length + b.length + 1 > width ? [...init, last, b] : [...init, `${last} ${b}`]
-    }, [''])
-    .join('\n')
-    .slice(1)
+    return text
+        .split(/\s/)
+        .reduce(
+            (a, b) => {
+                const init = a.slice(0, -1);
+                const [last] = a.slice(-1);
+                return last.length + b.length + 1 > width ? [...init, last, b] : [...init, `${last} ${b}`]
+            },
+            ['']
+        )
+        .join('\n')
+        .slice(1);
 }
--- scraper.js
+++ scraper.js
@@ -1,128 +1,144 @@
-import request from 'request'
-import $ from 'cheerio'
-import r from 'rethinkdb'
-import init from 'rethinkdb-init'
+import request from 'request';
+import $ from 'cheerio';
+import r from 'rethinkdb';
+import init from 'rethinkdb-init';
 
-const FACTS_PER_PAGE = 10
-const DB_NAME = 'factslides'
+const FACTS_PER_PAGE = 10;
+const DB_NAME = 'factslides';
 const connectionConfig = {
     host: 'localhost',
     port: 28015,
     pathname: '/',
     db: 'factslides'
-}
+};
 
-init(r)
-r.init(connectionConfig, ['facts'])
- .then(processPages)
- .then(done)
- .catch(processError)
+init(r);
+r
+    .init(connectionConfig, ['facts'])
+    .then(processPages)
+    .then(done)
+    .catch(processError);
 
 function processPages(connection) {
     return determineLastPage(connection).then(lastPage => {
-        if (lastPage < 2) done()
-        let pages = []
+        if (lastPage < 2) done();
+        const pages = [];
         for (let page = 2; page <= lastPage; page++) {
             pages.push(schedulePageProcessing(page)
                 .then(storeFacts(connection))
                 .then(pageProcessed(page)))
         }
         return Promise.all(pages)
-    })
+    });
 }
 
 function determineLastPage(connection) {
-    return r.table('facts').max('id').default({id: 0})
-            .run(connection)
-            .then(newestDbFact =>
-                schedulePageProcessing(1).then(facts => {
-                    let newestFactId = facts.map(f => f.id)
-                                            .reduce((a, b) => Math.max(a, b))
-                    let newestDbFactId = newestDbFact.id
-                    let lastPage = Math.ceil((newestFactId - newestDbFactId) / FACTS_PER_PAGE)
-                    if (lastPage != 0) {
+    return r
+        .table('facts')
+        .max('id')
+        .default({id: 0})
+        .run(connection)
+        .then(newestDbFact =>
+            schedulePageProcessing(1).then(facts => {
+                    let newestFactId = facts
+                        .map(f => f.id)
+                        .reduce((a, b) => Math.max(a, b));
+                    let newestDbFactId = newestDbFact.id;
+                    let lastPage = Math.ceil((newestFactId - newestDbFactId) / FACTS_PER_PAGE);
+                    if (lastPage !== 0) {
                         storeFacts(connection)(facts).then(pageProcessed(1))
-                                                     .catch(processError)
+                            .catch(processError)
                     }
-                    return lastPage
-                }))
+                    return lastPage;
+                }
+            )
+        );
 }
 
 function schedulePageProcessing(pageNumber) {
-    return processPage(`http://www.factslides.com/p-${pageNumber}`)
+    return processPage(`http://www.factslides.com/p-${pageNumber}`);
 }
 
 function processPage(url) {
     return new Promise((resolve, reject) =>
         request(url, (error, response, html) => {
-            if (error) return reject(error)
-            extractFacts($.load(html)).then(resolve)
-                                      .catch(reject)
-        }))
+            if (error) return reject(error);
+            extractFacts($.load(html))
+                .then(resolve)
+                .catch(reject)
+        }));
 }
 
 function extractFacts(document) {
     return new Promise((resolve, reject) => {
         try {
-            let data = document('script').filter((i, e) => Object.keys($(e)[0].attribs).length == 1)
-                                         .first().text()
-            let ids = extractArray(data, 'itemsID').slice(1)
-            let texts = extractFactTexts(document)
-            if (!(ids && texts)) return resolve([])
-            resolve(ids.map((id, i) => ({id: parseInt(id), text: texts[i]})))
+            const data = document('script')
+                .filter((i, e) =>
+                    Object.keys($(e)[0].attribs).length === 1
+                )
+                .first()
+                .text();
+            const ids = extractArray(data, 'itemsID').slice(1);
+            const texts = extractFactTexts(document);
+            if (!(ids && texts)) return resolve([]);
+            resolve(ids.map((id, i) => ({id: parseInt(id), text: texts[i]})));
         } catch (error) {
-            reject(error)
+            reject(error);
         }
-    })
+    });
 }
 
 function extractArray(script, arrayName) {
-    let match = new RegExp(`var ${arrayName}\\s*=\\s*(new Array\\(.+\\));`, 'g')
-        .exec(script)
-    if (!match) return []
-    return eval(match[1])
+    let match = new RegExp(`var ${arrayName}\\s*=\\s*(new Array\\(.+\\));`, 'g').exec(script);
+    if (!match) return [];
+    return eval(match[1]);
 }
 
 
 function extractFactTexts($) {
-    return $('#items .i').not('#fe').map((i, e) =>
-        normalize($(e).text())
-    ).get()
+    return $('#items .i')
+        .not('#fe')
+        .map((i, e) =>
+            normalize($(e).text())
+        )
+        .get();
 }
 
 function normalize(factText) {
-    return factText.trim()
-                   .replace(/\s+/g, ' ')
-                   .replace(/\s(\.|,)/g, '$1')
+    return factText
+        .trim()
+        .replace(/\s+/g, ' ')
+        .replace(/\s(\.|,)/g, '$1');
 }
 
 function storeFacts(connection) {
     return facts => new Promise((resolve, reject) => {
-        if (!facts) return resolve({})
-        r.table('facts')
-         .insert(facts)
-         .run(connection, (error, result) => {
-             if (error) return reject(error)
-             resolve(result)
-         })
-    })
+        if (!facts) return resolve({});
+        r
+            .table('facts')
+            .insert(facts)
+            .run(connection, (error, result) => {
+                if (error) return reject(error);
+                resolve(result);
+            });
+    });
 }
 
 function done() {
-    console.log('Done.')
-    process.exit(0)
+    console.log('Done.');
+    process.exit(0);
 }
 
 function processError(error) {
-    console.error(error.msg ? error.msg : error)
-    process.exit(1)
+    console.error(error.msg ? error.msg : error);
+    process.exit(1);
 }
 
 function pageProcessed(pageNumber) {
     return new Promise((resolve) => {
-        console.log(`Page ${pageNumber} processed.`)
-        resolve()
-    })
+        console.log(`Page ${pageNumber} processed.`);
+        resolve();
+    });
 }
 
 
